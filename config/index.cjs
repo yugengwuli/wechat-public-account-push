7b399f3329e7c84206d7b13b04ecae62885bec93
Waker Kid <50290360+yugengwuli@users.noreply.github.com>
2023-08-10 23:36:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9H6T6shCNQPc5rD71kHkOA6pais',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'NJ5y0wAFx97M8PL_n7qvt8LI3UgPjbi0EE_fx6m35cw',
+      useTemplateId: 'RLItIJHS-FYRcC49FlIwryJsTTB_kduB-m9S7DIoRYI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-08',
       festivals: [
